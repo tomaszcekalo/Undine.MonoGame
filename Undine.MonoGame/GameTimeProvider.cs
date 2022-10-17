551b3556d46bf7dec50000086930fe6242eb4315
Tomasz Cekało <tomasz.cekalo@gmail.com>
2022-10-17 14:12:18 +0200

    added game time provider

---
+++ Undine.MonoGame/GameTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Undine.MonoGame
+{
+    public interface IGameTimeProvider
+    {
+        GameTime GameTime { get; set; }
+    }
+
+    public class GameTimeProvider : IGameTimeProvider
+    {
+        public GameTime GameTime { get; set; }
+    }
+}
